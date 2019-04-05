da3c9902153e773e5ad012a97f68bed042bd32c9
unknown <793736331@qq.com>
2019-04-05 09:12:56 +0800

    dva project

---
+++ src/components/Users/Users.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Button } from 'antd';
+import { Table, Pagination, Popconfirm } from 'antd';
+import 'antd/lib/button/style/css';
+import 'antd/lib/table/style/css';
+import styles from './Users.css';
+import { PAGE_SIZE } from '../../../constants';
+
+
+function Users({ list: dataSource, total, page: current }) {
+  function deleteHandler(id) {
+    console.warn(`TODO: ${id}`);
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a href="">{text}</a>,
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Website',
+      dataIndex: 'website',
+      key: 'website',
+    },
+    {
+      title: 'Operation',
+      key: 'operation',
+      render: (text, { id }) => (
+        <span className={styles.operation}>
+          <a href="">Edit</a>
+          <Popconfirm title="Confirm to delete?" onConfirm={deleteHandler.bind(null, id)}>
+            <a href="">Delete</a>
+          </Popconfirm>
+        </span>
+      ),
+    },
+  ];
+
+  return (
+    <div className={styles.normal}>
+      <div>
+        <Button type="primary">添加用户</Button>
+      </div>
+      <br/>
+      <div>
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          rowKey={record => record.id}
+          pagination={false}
+        />
+        <Pagination
+          className="ant-table-pagination"
+          total={total}
+          current={current}
+          pageSize={PAGE_SIZE}
+        />
+      </div>
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  const { list, total, page } = state.users;
+  return {
+    list,
+    total,
+    page,
+  };
+}
+
+export default connect(mapStateToProps)(Users);
